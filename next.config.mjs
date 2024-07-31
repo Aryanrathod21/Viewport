851486cd258d94638eb5ea5db42167547d7d5e8f
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-07-31 17:24:53 +0530

    final

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+import withVideos from 'next-videos';
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default withVideos(nextConfig);
